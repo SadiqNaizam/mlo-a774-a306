b6793be2aa67482e8b86598b435e8477f1b53914
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-25 20:20:06 +0530

    Updated code files: MainAppLayout.tsx

--- src/components/layout/MainAppLayout.tsx
+++ src/components/layout/MainAppLayout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+```tsx
+import React, { useState } from 'react';
 import SidebarNav from './SidebarNav';
 import TopHeader from './TopHeader';
 import { cn } from '@/lib/utils';
@@ -9,26 +10,51 @@ interface MainAppLayoutProps {
 }
 
 const MainAppLayout: React.FC<MainAppLayoutProps> = ({ children, className }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className={cn("grid h-screen w-full grid-cols-[288px_1fr] grid-rows-[64px_1fr] bg-background", className)}>
-      {/* Sidebar */}
-      <div className="row-span-2 border-r border-sidebar-foreground/10 bg-sidebar">
-        <SidebarNav />
+    <div className={cn("h-screen w-full bg-background", className)}>
+      {/* Sidebar: fixed for mobile overlay, static for desktop */}
+      <div
+        className={cn(
+          "fixed inset-y-0 left-0 z-50 h-full w-[288px] transform bg-sidebar transition-transform duration-300 ease-in-out lg:translate-x-0",
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        )}
+      >
+        <div className="h-full border-r border-sidebar-foreground/10">
+          <SidebarNav />
+        </div>
       </div>
 
-      {/* Header */}
-      <header className="col-start-2">
-        <TopHeader />
-      </header>
+      {/* Overlay to close sidebar on mobile */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 z-40 bg-black/50 lg:hidden"
+          onClick={toggleSidebar}
+        />
+      )}
 
-      {/* Main Content */}
-      <main className="col-start-2 row-start-2 overflow-y-auto">
-        <div className="p-6">
-          {children}
-        </div>
-      </main>
+      {/* Main content area */}
+      <div className="flex h-full flex-col lg:pl-[288px]">
+        {/* Header */}
+        <header className="flex-shrink-0">
+          <TopHeader onMenuClick={toggleSidebar} />
+        </header>
+
+        {/* Main Content */}
+        <main className="flex-1 overflow-y-auto">
+          <div className="p-6">
+            {children}
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
 
 export default MainAppLayout;
+```
